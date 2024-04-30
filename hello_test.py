8919a4cd88c48bb47fccaa80193acbb1bbaea851
tmkop <61564939+tmkop@users.noreply.github.com>
2024-04-30 16:55:12 -0400

    Update hello_test.py

--- hello_test.py
+++ hello_test.py
@@ -1,4 +1,11 @@
 import hello;
-
-def test_hello():
+    
+def test():
     assert hello.hello_world() == "Hello World!", "This should be 'Hello World!'"
+
+if __name__ == "__main__":
+    try:
+        test()
+        print("All tests passed")  # Print success message after tests have run
+    except AssertionError as e:
+        print("Test failed:", e)
